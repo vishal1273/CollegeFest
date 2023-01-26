99026efa5ee8734c1a2c228d81c74f4e0169af47
vishal1273 <vishalthakur107@gmail.com>
2023-01-27 02:53:19 +0530

    configure property file for Spring DataSource, Hibernate Properties

---
+++ src/main/java/com/gl/collegefest/CollegeFestApplication.java
@@ -0,0 +1,13 @@
+package com.gl.collegefest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeFestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeFestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gl/collegefest/CollegeFestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gl.collegefest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeFestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
